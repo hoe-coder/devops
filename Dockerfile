ab6a467094b7a19b5e0bdf99d5242e2839453f98
hoe-coder <hoe8742@students.fhv.at>
2025-09-26 09:58:17 +0200

    docker build

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,16 @@
-FROM hello-world
+FROM gradle:9.1.0-jdk-21-and-24 AS build
+
+WORKDIR /app
+COPY . .
+
+RUN gradle clean test --no-deamon
+
+RUN gradle bootJar --no-deamon
+
+FROM eclipse-temurin:21-jdk
+
+WORKDIR /app
+COPY --from=build /app/build/libs/*.jar app.jar
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.devops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class DevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(2 + 2).isEqualTo(4);
 	}
 
 }
