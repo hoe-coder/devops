24b2900bdabf2adabe9708d57645feda435c9ae5
ati <ace4804@students.fhv.at>
2025-10-10 09:23:31 +0200

    sonar-scanner pipeline push

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,16 @@ plugins {
 	java
 	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("jacoco")
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-description = "Devops non-detailed sample project"
+description = "DevOps Lecture 5 – Code Analysis with SonarQube"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion.set(JavaLanguageVersion.of(21))
 	}
 }
 
@@ -22,13 +23,30 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
 	runtimeOnly("com.h2database:h2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy("jacocoTestReport")
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true) // notwendig für SonarQube
+		csv.required.set(false)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+	}
+}
+
+jacoco {
+	toolVersion = "0.8.12"
 }
